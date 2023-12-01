25068e05385bbaf0f7c517d6d59ca208694227f0
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-11-30 20:38:48 -0500

    update app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+          <Header />
+          <Sidebar />
+          <Footer />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
+ 
